c46d0843f39a4289101ad765e0db112743b66c23
Nilay Sharma <nilaysharma642@gmail.com>
2019-05-14 23:18:03 +0530

    Updates

---
+++ precache-manifest.4dce3ca1ef11276a6b5ebb9c3a3f790e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1230a8df2a82811f0ce6b61d102a4a2",
+    "url": "/pipabella-listing-page/index.html"
+  },
+  {
+    "revision": "9413240b674e77a2e866",
+    "url": "/pipabella-listing-page/static/css/main.96c24bb0.chunk.css"
+  },
+  {
+    "revision": "0d06c4f84619b0494778",
+    "url": "/pipabella-listing-page/static/js/2.1268833f.chunk.js"
+  },
+  {
+    "revision": "9413240b674e77a2e866",
+    "url": "/pipabella-listing-page/static/js/main.3f063b69.chunk.js"
+  },
+  {
+    "revision": "ad87457c20cf3852cab2",
+    "url": "/pipabella-listing-page/static/js/runtime~main.62314323.js"
+  },
+  {
+    "revision": "b054d328b95def2e4c02a30bde659cc9",
+    "url": "/pipabella-listing-page/static/media/download.b054d328.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pipabella-listing-page/precache-manifest.4dce3ca1ef11276a6b5ebb9c3a3f790e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pipabella-listing-page/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
